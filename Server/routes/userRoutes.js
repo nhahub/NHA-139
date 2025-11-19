0c1f9076d2c7cebf18b5b0ad500786810d5bc848
MohamedHossamRT <mohamedhossamelamrawi@gmail.com>
2025-11-19 20:06:49 +0200

    Fixed the Routes order in userRoutes

--- Server/routes/userRoutes.js
+++ Server/routes/userRoutes.js
@@ -3,16 +3,6 @@ const router = express.Router();
 const userController = require("../controllers/userController");
 const { protect, restrictTo } = require("../middlewares/authMiddleware");
 
-// Admin Routes
-router
-  .route("/")
-  .get(protect, restrictTo("admin"), userController.getAllUsers)
-  .post(protect, restrictTo("admin"), userController.addUser);
-router
-  .route("/:id")
-  .put(protect, restrictTo("admin"), userController.updateUser)
-  .delete(protect, restrictTo("admin"), userController.deleteUser);
-
 // User profile routes
 router.get("/me", protect, userController.getMyProfile);
 router.put("/update", protect, userController.updateProfile);
@@ -29,5 +19,15 @@ router
   .route("/history")
   .post(protect, userController.addToHistory)
   .delete(protect, userController.clearHistory);
+
+// Admin Routes
+router
+  .route("/")
+  .get(protect, restrictTo("admin"), userController.getAllUsers)
+  .post(protect, restrictTo("admin"), userController.addUser);
+router
+  .route("/:id")
+  .put(protect, restrictTo("admin"), userController.updateUser)
+  .delete(protect, restrictTo("admin"), userController.deleteUser);
 
 module.exports = router;
